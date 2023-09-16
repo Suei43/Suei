00ef58167d30feaece460a2e395b39744ecb4504
darksuei <chrisraphael576@gmail.com>
2023-09-16 18:08:22 +0100

    Chore: Refactor icons

--- src/components/LeftNav.tsx
+++ src/components/LeftNav.tsx
@@ -1,120 +1,21 @@
-"use client"
-import { useEffect, useState } from "react"
+"use client";
+import { useEffect, useState } from "react";
+import { Discord, Instagram, LinkedIn } from "./icons";
+import { Github } from "./icons";
+import Twitter from "./icons/Twitter";
 
-
-export default function LeftNav(){
-    const [activeIndex, setActiveIndex] = useState(-1) 
-
-    return (
-        <div className={`w-12 md:w-16 flex flex-col-reverse nav-left absolute md:fixed box-border left-0 top-0 z-10`}>
-            <div className="w-full flex flex-col items-center justify-center gap-y-10 py-24 md:py-12">
-                <a href="https://discordapp.com/users/sueii">
-                    <svg 
-                        width="28" 
-                        height="28" 
-                        viewBox="0 0 192 192" 
-                        xmlns="http://www.w3.org/2000/svg" 
-                        fill="none"
-                        className="social-icon"
-                        onMouseEnter={() => setActiveIndex(0)}
-                        onMouseLeave={() => setActiveIndex(-1)}
-                    >
-                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-                        <g id="SVGRepo_iconCarrier">
-                            <path className="social-icon" stroke={ activeIndex === 0 ? '#0579C3' : '#8892b0' } stroke-linecap="round" stroke-linejoin="round" stroke-width="9.6" d="m68 138-8 16c-10.19-4.246-20.742-8.492-31.96-15.8-3.912-2.549-6.284-6.88-6.378-11.548-.488-23.964 5.134-48.056 19.369-73.528 1.863-3.334 4.967-5.778 8.567-7.056C58.186 43.02 64.016 40.664 74 39l6 11s6-2 16-2 16 2 16 2l6-11c9.984 1.664 15.814 4.02 24.402 7.068 3.6 1.278 6.704 3.722 8.567 7.056 14.235 25.472 19.857 49.564 19.37 73.528-.095 4.668-2.467 8.999-6.379 11.548-11.218 7.308-21.769 11.554-31.96 15.8l-8-16m-68-8s20 10 40 10 40-10 40-10"></path>
-                            <ellipse className="social-icon" cx="71" cy="101" fill={ activeIndex === 0 ? '#0579C3' : '#8892b0' } rx="13" ry="15"></ellipse>
-                            <ellipse className="social-icon" cx="121" cy="101" fill={ activeIndex === 0 ? '#0579C3' : '#8892b0' } rx="13" ry="15"></ellipse>
-                        </g>
-                    </svg>
-                </a>
-
-                <a href="https://github.com/darksuei">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke={ activeIndex === 1 ? '#0579C3' : '#8892b0' }
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        className="social-icon"
-                        onMouseEnter={() => setActiveIndex(1)}
-                        onMouseLeave={() => setActiveIndex(-1)}
-                    >
-                        <path
-                            d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
-                        ></path>
-                    </svg>
-                </a>
-
-                <a href="https://instagram.com/darksuei">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke={ activeIndex === 2 ? '#0579C3' : '#8892b0' }
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        className="social-icon"
-                        onMouseEnter={() => setActiveIndex(2)}
-                        onMouseLeave={() => setActiveIndex(-1)}
-                    >
-                        <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
-                        <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
-                        <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
-                    </svg>
-                </a>
-
-                <a href="https://www.linkedin.com/in/folarin-raphael">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke={ activeIndex === 3 ? '#0579C3' : '#8892b0' }
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        className="social-icon"
-                        onMouseEnter={() => setActiveIndex(3)}
-                        onMouseLeave={() => setActiveIndex(-1)}
-                    >
-                        <path
-                            d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"
-                        ></path>
-                        <rect x="2" y="9" width="4" height="12"></rect>
-                        {/* <circle cx="4" cy="4" r="2"></circle> */}
-                    </svg>
-                </a>
-
-                <a href='https://twitter.com/sueii__'>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke={ activeIndex === 4 ? '#0579C3' : '#8892b0' }
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        className="social-icon"
-                        onMouseEnter={() => setActiveIndex(4)}
-                        onMouseLeave={() => setActiveIndex(-1)}
-                    >
-                        <path
-                            d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"
-                        ></path>
-                    </svg>
-                </a>
-            </div>
-        </div>
-    )
+export default function LeftNav() {
+  return (
+    <div
+      className={`w-12 md:w-16 flex flex-col-reverse nav-left absolute md:fixed box-border left-0 top-0 z-10`}
+    >
+      <div className="w-full flex flex-col items-center justify-center gap-y-10 py-24 md:py-12">
+        <Discord />
+        <Github />
+        <Instagram />
+        <LinkedIn />
+        <Twitter />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/icons/index.ts
@@ -0,0 +1,7 @@
+import Github from "./Github";
+import Instagram from "./Instagram";
+import Preview from "./Preview";
+import LinkedIn from "./LinkedIn";
+import Discord from "./Discord";
+
+export { Discord, Github, Instagram, Preview, LinkedIn };
