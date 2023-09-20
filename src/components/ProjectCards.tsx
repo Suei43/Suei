05425880dd6e98ec43d984d337cf42f4304c527a
darksuei <chrisraphael576@gmail.com>
2023-09-20 23:22:15 +0100

    Update projects

--- src/components/ProjectCards.tsx
+++ src/components/ProjectCards.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { moreprojects } from "@/data";
+import { Github, Preview } from "./icons";
 
 const ProjectCards = () => {
   const [viewMore, setViewMore] = useState(false);
@@ -21,53 +22,8 @@ const ProjectCards = () => {
             key={project.id}
           >
             <div className="flex flex-row items-center justify-end gap-x-3 w-full">
-              <a href={project.git}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="18"
-                  height="18"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  id={project.id}
-                  stroke={
-                    activeIndex === String(project.id) + 1
-                      ? "#0579C3"
-                      : "#8892b0"
-                  }
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="social-icon"
-                  onMouseEnter={() => setActiveIndex(String(project.id) + 0)}
-                  onMouseLeave={() => setActiveIndex("")}
-                >
-                  <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
-                </svg>
-              </a>
-              <a href={project.link}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="21"
-                  height="21"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke={
-                    activeIndex === String(project.id) + 1
-                      ? "#0579C3"
-                      : "#8892b0"
-                  }
-                  strokeWidth="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  className="social-icon"
-                  onMouseEnter={() => setActiveIndex(String(project.id) + 1)}
-                  onMouseLeave={() => setActiveIndex("")}
-                >
-                  <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
-                  <polyline points="15 3 21 3 21 9" />
-                  <line x1="10" y1="14" x2="21" y2="3" />
-                </svg>
-              </a>
+              <Github link={project.git} />
+              <Preview link={project.link} />
             </div>
             <h1>{project.name}</h1>
             <p className="text-primary text-xs">{project.description}</p>
--- src/data.ts
+++ src/data.ts
@@ -1,12 +1,13 @@
 import cloudwave from "@/assets/cloudwave.png";
 import cybtracy from "@/assets/cybtracy.png";
 import raphaelly from "@/assets/raphaelly.png";
+import mega from "@/assets/mega.png";
 
 export const experience = [
   {
     id: 1,
     company: "Oluadepe Farms Ltd.",
-    title: "QA Tester Intern",
+    title: "Software Tester Intern",
     range: "Jun 2023 - Present",
     url: "",
     btn: 1,
@@ -18,7 +19,7 @@ export const experience = [
   {
     id: 2,
     company: "Circle-Dodge Systems",
-    title: "Back-end Developer",
+    title: "BackEnd Developer",
     range: "Feb 2023 - Aug 2023",
     url: "https://circle-dodge.com",
     btn: 2,
@@ -39,36 +40,36 @@ export const experience = [
       "Worked on the social media and marketing team to promote the organization's mission and vision of promoting digital literacy among kids in underserved communities.",
     ],
   },
+  // {
+  //   id: 4,
+  //   company: "",
+  //   title: "A2SV Hackathon semi-finalist",
+  //   range: "Sep 2023",
+  //   url: "",
+  //   btn: 4,
+  //   description: [
+  //     "Led a team of 4 to develop a web application that aids students in exam preparation using Generative AI.",
+  //     "Pitched the idea to a panel of judges and investors",
+  //   ],
+  // },
   {
     id: 4,
-    company: "",
-    title: "A2SV Hackathon Semi-finalist",
-    range: "Sep 2023",
-    url: "",
-    btn: 4,
-    description: [
-      "Led a team of 4 to develop a web application that aids students in exam preparation using Generative AI.",
-      "Pitched the idea to a panel of judges and investors",
-    ],
-  },
-  {
-    id: 5,
     company: "Google Developers Student Community",
     title: "GDSC Core Team Member",
     range: "Aug 2023",
     url: "",
-    btn: 5,
+    btn: 4,
     description: [
       "Actively engaging the student community by hosting events and talk sessions",
     ],
   },
   {
-    id: 6,
+    id: 5,
     company: "alx_africa",
     title: "Software Engineering Trainee",
     range: "Jun 2023",
     url: "",
-    btn: 6,
+    btn: 5,
     description: [
       "Learnt the fundamentals of software development including low level programming, data structures and algorithms, and object oriented programming.",
       "Worked in teams to complete projects using Agile methodologies.",
@@ -91,28 +92,28 @@ export const projects = [
     name: "Image Vision API",
     id: 2,
     description:
-      "An image classification API made using tensorflow mobile-net model for image recognition.",
+      "An image recognition API created using the tensorflow mobile-net computer vision model for image recognition.",
     tech: ["TypeScript", "Fastify", "TypeORM", "Tensorflow JS", "Mongo DB"],
     link: "https://image-vision-api.onrender.com/",
     image: "",
     git: "https://github.com/darksuei/imagevision",
   },
   {
-    name: "Cybtracy",
+    name: "Mega JS Documentation",
     id: 3,
     description:
-      "The official website for Cybtracy, a non-profit organization that aims to provide digital literacy to children in underserved communities.",
-    tech: ["Next JS", "Web pack", "TypeScript", "Framer Motion"],
-    link: "https://cybtracy.com/",
-    image: cybtracy,
-    git: "https://github.com/Cybtracy/webapp-frontend",
+      "The documentation for MEGA JS. An unofficial Javascript sdk for MEGA.nz",
+    tech: ["JavaScript", "Docusaurus", "Markdown"],
+    link: "https://mega.js.org/",
+    image: mega,
+    git: "https://github.com/qgustavor/megajs-docs",
   },
   {
     name: "Raphaelly Editorials Services",
     id: 4,
     description:
       "The official website for an editorial organization focused on project research and marketing consultancy.",
-    tech: ["JavaScript", "Tailwind CSS", "Django", "PostgreSQL", "AWS S3"],
+    tech: ["JavaScript", "Tailwind CSS", "Django", "PostgreSQL"],
     link: "https://raphaellyeditorial.netlify.app/",
     image: raphaelly,
     git: "https://github.com/darksuei/raphaellyeditorials",
@@ -129,17 +130,19 @@ export const moreprojects = [
     git: "https://github.com/hellofaizan/xprofile",
   },
   {
-    name: "Mega JS Documentation",
+    name: "Cybtracy",
     id: 2,
-    description: "The official documentation for the Mega JS library",
-    tech: ["JavaScript", "Docusaurus", "Markdown"],
-    link: "https://mega.js.org/",
-    git: "https://github.com/qgustavor/megajs-docs",
+    description:
+      "The official website for Cybtracy, a non-profit organization that aims to provide digital literacy to children in underserved communities.",
+    tech: ["Next JS", "Web pack", "TypeScript", "Framer Motion"],
+    link: "https://cybtracy.com/",
+    image: cybtracy,
+    git: "https://github.com/Cybtracy/webapp-frontend",
   },
   {
     name: "My Portfolio v1",
     id: 3,
-    description: "First version of my portfolio website",
+    description: "The first version of my portfolio website.",
     tech: ["TypeScript", "Next JS", "Tailwind CSS"],
     link: "https://suei.space",
     git: "https://github.com/darksuei/suei",
@@ -147,7 +150,8 @@ export const moreprojects = [
   {
     name: "Studera AI",
     id: 4,
-    description: "A personalized exam preparation platform",
+    description:
+      "A personalized exam preparation web application (A2SV Generative Hackathon Solution).",
     tech: ["Angular", "Redis", "Nest JS", "MongoDB"],
     link: "https://studera.onrender.com",
     git: "https://github.com/darksuei/studera-AI",
@@ -155,7 +159,7 @@ export const moreprojects = [
   {
     name: "Uchegbu Family Tree",
     id: 5,
-    description: "Edit and explore your family tree.",
+    description: "Document and save your family tree for future generations.",
     tech: ["JavaScript", "D3 JS", "Mongo DB", "Node JS"],
     link: "https://uchegbu-family-tree.onrender.com/",
     git: "https://github.com/darksuei/family-tree",
@@ -163,7 +167,8 @@ export const moreprojects = [
   {
     name: "Shell Scripts",
     id: 6,
-    description: "A collection of shell scripts for automating git workflows",
+    description:
+      "A collection of shell scripts for automating my git workflows.",
     tech: ["Bash Scripting", "Linux Shell"],
     link: "#",
     git: "https://github.com/darksuei/scripts",
